f73b88f5ff14069775e782017b99183452e549ad
sferdary <s.ferdary@pm.me>
2018-09-04 20:15:43 +0200

    Java opdrachten

--- src/h06/Opdracht6_2.java
+++ src/h06/Opdracht6_2.java
@@ -1,4 +1,45 @@
 package h06;
 
-public class Opdracht6_2 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht6_2 extends Applet {
+    int TotaalBedrag, AantalPersonen, DeelBedrag;
+
+    public void init() {
+        setBackground(Color.white);
+        TotaalBedrag = 113;
+        AantalPersonen = 4;
+        DeelBedrag = TotaalBedrag / AantalPersonen;
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.drawString("Geld verdeling:", 10, 30);
+        g.drawString("€ " + TotaalBedrag, 100, 30);
+
+        //Vertical lines
+        g.drawLine(10, 45, 10, 125);
+        g.drawLine(70, 45, 70, 125);
+        g.drawLine(140, 45, 140, 125);
+
+        //Horizontal lines
+        g.drawLine(10, 45, 140, 45);
+        g.drawLine(10, 65, 140, 65);
+        g.drawLine(10, 85, 140, 85);
+        g.drawLine(10, 105, 140, 105);
+        g.drawLine(10, 125, 140, 125);
+
+        g.drawString("Ali", 15, 60);
+        g.drawString("€ " + DeelBedrag, 80, 60);
+
+        g.drawString("Jan", 15, 80);
+        g.drawString("€ " + DeelBedrag, 80, 80);
+
+        g.drawString("Jeannette", 15, 100);
+        g.drawString("€ " + DeelBedrag, 80, 100);
+
+        g.drawString("Samuel", 15, 120);
+        g.drawString("€ " + DeelBedrag, 80, 120);
+    }
 }
--- src/h06/Opdracht6_3.java
+++ src/h06/Opdracht6_3.java
@@ -1,4 +1,33 @@
 package h06;
 
-public class Opdracht6_3 {
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht6_3 extends Applet {
+    int GetalEen, GetalTwee, Antwoord;
+
+    public void init() {
+        setBackground(Color.white);
+        GetalEen = 2147483647;
+        GetalTwee = 2147483647;
+        Antwoord = GetalEen + GetalTwee;
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.black);
+        g.drawString("Negatieve optelsom:", 10, 30 );
+
+        g.drawString("Getal 1:", 15, 60 );
+        g.drawString(GetalEen + "", 80, 60 );
+
+        g.drawString("Getal 2:", 15, 80 );
+        g.drawString(GetalTwee + "", 80, 80 );
+
+        //Horizontal lines
+        g.drawLine(10,85,175,85);
+        g.drawString("+", 170, 80 );
+
+        g.drawString("Totaal:", 15, 100 );
+        g.drawString(Antwoord + "", 80, 100 );
+    }
 }
