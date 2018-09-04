4cdc05b48479ed1904781b0c44f7c7f0c20a401f
sferdary <s.ferdary@pm.me>
2018-09-04 20:55:27 +0200

    Java opdrachten

--- src/h06/Opdracht6_2.java
+++ src/h06/Opdracht6_2.java
@@ -4,42 +4,42 @@ import java.awt.*;
 import java.applet.*;
 
 public class Opdracht6_2 extends Applet {
-    int TotaalBedrag, AantalPersonen, DeelBedrag;
+    int Seconde, Uur, Dag, Jaar;
 
     public void init() {
         setBackground(Color.white);
-        TotaalBedrag = 113;
-        AantalPersonen = 4;
-        DeelBedrag = TotaalBedrag / AantalPersonen;
+        Seconde = 1;
+        Uur = Seconde * 3600;
+        Dag = Uur * 24;
+        Jaar = Dag *365;
     }
 
     public void paint(Graphics g) {
         g.setColor(Color.black);
-        g.drawString("Geld verdeling:", 10, 30);
-        g.drawString("€ " + TotaalBedrag, 100, 30);
+        g.drawString("Tijdseenheden in seconden :", 10, 30);
 
         //Vertical lines
-        g.drawLine(10, 45, 10, 125);
-        g.drawLine(70, 45, 70, 125);
-        g.drawLine(140, 45, 140, 125);
+        g.drawLine(10, 45, 10, 105);
+        g.drawLine(70, 45, 70, 105);
+        g.drawLine(210, 45, 210, 105);
 
         //Horizontal lines
-        g.drawLine(10, 45, 140, 45);
-        g.drawLine(10, 65, 140, 65);
-        g.drawLine(10, 85, 140, 85);
-        g.drawLine(10, 105, 140, 105);
-        g.drawLine(10, 125, 140, 125);
+        g.drawLine(10, 45, 210, 45);
+        g.drawLine(10, 65, 210, 65);
+        g.drawLine(10, 85, 210, 85);
+        g.drawLine(10, 105, 210, 105);
 
-        g.drawString("Ali", 15, 60);
-        g.drawString("€ " + DeelBedrag, 80, 60);
+        g.drawString("Uur", 15, 60);
+        g.drawString(Uur + "", 80, 60);
+        g.drawString("seconden", 150, 60);
 
-        g.drawString("Jan", 15, 80);
-        g.drawString("€ " + DeelBedrag, 80, 80);
+        g.drawString("Dag", 15, 80);
+        g.drawString(Dag + "", 80, 80);
+        g.drawString("seconden", 150, 80);
 
-        g.drawString("Jeannette", 15, 100);
-        g.drawString("€ " + DeelBedrag, 80, 100);
+        g.drawString("Jaar", 15, 100);
+        g.drawString(Jaar + "", 80, 100);
+        g.drawString("seconden", 150, 100);
 
-        g.drawString("Samuel", 15, 120);
-        g.drawString("€ " + DeelBedrag, 80, 120);
     }
 }
