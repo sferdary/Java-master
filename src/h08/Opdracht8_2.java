d0f76b118935e9a94c1bf961aadd285aac466f9e
sferdary <s.ferdary@pm.me>
2018-09-05 17:35:06 +0200

    Java opdrachten

--- src/h08/Opdracht8_2.java
+++ src/h08/Opdracht8_2.java
@@ -1,4 +1,38 @@
 package h08;
 
-public class Opdracht8_2 {
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+
+public class Opdracht8_2 extends Applet {
+    Button knopBezMan;
+    Button knopPotMan;
+    Button knopBezVrouw;
+    Button knopPotVrouw;
+    Label label;
+    String reSet, invoerBezMan;
+    double getal;
+
+    public void init() {
+        setSize(600, 300);
+        label = new Label("Voer aantallen in");
+
+        knopBezMan = new Button();
+        knopBezMan.setLabel("Man");
+        knopBezMan.addActionListener(new knopBezManListener());
+        invoerBezMan = knopBezMan;
+        add(knopBezMan);
+    }
+
+    class knopBezManListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            invoerBezMan = "";
+            repaint();
+        }
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("Aantal mannelijke bezoekers", 20, 115);
+        g.drawString(": " + invoerBezMan, 250, 115);
+    }
 }
