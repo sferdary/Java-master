451a142a9eb6196546cc47eebec1a7bed8ecb9f8
sferdary <s.ferdary@pm.me>
2018-10-01 10:36:43 +0200

    Java opdrachten

--- src/h11/Opdracht11_3.java
+++ src/h11/Opdracht11_3.java
@@ -4,18 +4,21 @@ import java.applet.Applet;
 import java.awt.*;
 
 public class Opdracht11_3 extends Applet {
+    Label labelHeader;
+    int intOne = 1, intTwo = 0;
+    int intTemp = 0;
+    int x = 10;
 
     public void init() {
+        setSize(350, 50);
+        labelHeader = new Label("Fibonacci:");
+        add(labelHeader);
     }
 
     public void paint(Graphics g) {
-        int intOne = 1, intTwo = 0;
-        int intTemp = 0;
-        int y = 0;
-
-        for (int i = 0; i < 10; i++) {
-           g.drawString(intOne + intTwo + " ", 20, y);
-           y += 20;
+        for (int i = 0; i < 13; i++) {
+            g.drawString(intOne + intTwo + " ", x, 40);
+            x += 25;
             intTemp = intOne;
             intOne = intTwo;
             intTwo = intTemp + intTwo;
